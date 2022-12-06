a0886a4df7a2be9e6ade56422c730cf9de75acb5
Hau Vu <65142251+Hau519@users.noreply.github.com>
2022-12-06 10:38:51 -0500

    shopping cart

---
+++ src/main/java/controllers/RemoveController.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import models.entities.ShoppingCart;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "RemoveController", value = "/Remove")
+public class RemoveController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        ArrayList<ShoppingCart> cartList = (ArrayList<ShoppingCart>) session.getAttribute("cartList");
+        if(cartList!=null) {
+            int index= Integer.parseInt(request.getParameter("index"));
+            cartList.remove(index);
+        }
+
+        request.getRequestDispatcher("WEB-INF/shoppingCart.jsp").forward(request, response);
+    }
+
+}
